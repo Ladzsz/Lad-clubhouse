af0628e1b4f615aa8c4da4408ef8e1da2a3eb9b7
Ladzsz <londonmason602@gail.com>
2026-05-12 16:43:44 -0400

    created email service file (untested)

---
+++ backend/src/utils/emailService.js
@@ -0,0 +1,44 @@
+import crypto from "crypto";
+import nodemailer from "nodemailer";
+
+// logging into app email account
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+//generate token for password reset
+export const generateResetToken = () => {
+    const token = crypto.randomBytes(32).toString("hex");
+
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(token)
+        .digest("hex");
+
+    const expires = Date.now() + 1000 * 60 * 60; // 1 hour
+
+    return { token, hashedToken, expires };
+};
+
+//send the email with reset link
+export const sendResetEmail = async (email, token) => {
+
+    const mailOptions = {
+        from: `"Support" <${process.env.EMAIL_USER}>`,
+        to: email,
+        subject: "Password Reset",
+        text: `Click here to reset your password: ${process.env.RESET_URL}/${token}`,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+    } catch (err) {
+        console.error("Error sending email:", err);
+        throw new Error("Failed to send reset email", { cause: err });
+    }
+
+};
