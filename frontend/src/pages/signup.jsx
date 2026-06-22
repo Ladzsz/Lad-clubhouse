ba006d545ecade60660f40e10404848d0ec30e48
Ladzsz <londonmason602@gail.com>
2026-06-22 15:09:21 -0400

    created signup form

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,8 +1,130 @@
-function Signup() {
+import { useState } from 'react';
+import '../assets/styles/signup.css'
+import { Link } from "react-router-dom";
+
+export function Signup() {
+  //setting form variables and states
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [error, setError] = useState('');
+
+  //handling form change
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  //handling form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    //password match error
+    if (formData.password !== formData.confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+    setError('');
+
+    //api call to create user
+    try {
+      const response = await fetch('http://localhost:5000/api/users/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: formData.username,
+          email: formData.email,
+          password: formData.password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Registration failed');
+      }
+
+      alert("User created!")
+
+    } catch (err) {
+      setError(err.message || 'Something went wrong');
+    }
+  };
+
+  //rendering form
   return (
-    <>
-      <h1>im the signup page</h1>
-    </>
+    <div className='form-container'>
+      <h2>Register</h2>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <form onSubmit={handleSubmit} method="POST" className='signup-form'>
+
+        {/*form inputs*/}
+        <div className='form-input-container'>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+            
+          />
+        </div>
+        
+        <div className='form-input-container'>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            
+          />
+        </div>
+
+        <div className='form-input-container'>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className='form-input-container'>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+      {/*button*/}
+        <button 
+          type="submit" 
+          className='form-btn'
+        >
+          Submit
+        </button>
+
+        {/*login link*/}
+        <Link to="/login">
+            <li className="login-link">go to login</li>
+          </Link>
+      </form>
+    </div>
   );
 }
 
