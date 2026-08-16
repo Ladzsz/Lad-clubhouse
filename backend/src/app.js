52104493f2165d3e36b9978330f0013d2086a48b
Ladzsz <londonmason602@gail.com>
2026-08-16 13:44:09 -0400

    updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+    'http://localhost:5173',
+    'https://ladscreech.netlify.app'
+  ],
     credentials: true,
   }),
 );
